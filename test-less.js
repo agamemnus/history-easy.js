618d769951fbd7b0852bde422efe22fab097bc5e
agamemnus <agamemnus@gmail.com>
2014-01-24 23:38:45 -0500

    Create test-less.js

---
+++ test-less.js
@@ -0,0 +1,11 @@
+var history_control = new history_control_object ({
+ 'onstatechange' : function (new_state, callback) {
+  document.body.innerHTML = new_state.fruit
+  callback ()
+ }
+})
+ 
+history_control.load_page (
+ {fruit: 'Current fruit: "apple". Changes to "pear" in 2 seconds.'},
+ {url: 'apple', title: 'Apple.', callback: function () {console.log ("The page changed.")}}
+)
